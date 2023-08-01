fe464ec6a823053a10a7a506230eba5e533a9667
takumi19 <54025184+takumi19@users.noreply.github.com>
2023-08-01 21:11:49 +0300

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  miniresume
+//
+//  Created by Kirill Azarenkov on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
